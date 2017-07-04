77424f67fa8440803047da5dfa966d5b899d58b0
LiXuefei <LF@llbt.local>
2017-07-04 16:40:36 +0800

    by lixuefei

---
+++ src/pages/account/MyInformation.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="my-information">
+        <div class="my-information-title">
+            <button>返回</button>
+            <h4>我的信息</h4>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    h4 {
+        font-size: 24px;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import MyInformation from '../pages/account/MyInformation'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/pages/account/MyInformation',
+      name: 'MyInformation',
+      component: MyInformation
     }
   ]
 })
