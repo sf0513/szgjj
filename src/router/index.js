1e4b8287ee8a4016bf5e9ee879c11363462f0366
luojian <luojian19910119@gmail.com>
2017-07-05 15:52:07 +0800

    添加登陆界面路由

--- src/router/index.js
+++ src/router/index.js
@@ -3,25 +3,31 @@ import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import MyInformation from '../pages/account/MyInformation'
 import Appointment from '../pages/appointment/appointment'
+import Login from '../pages/login/Login'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    },
-    {
-      path: '/pages/account/MyInformation',
-      name: 'MyInformation',
-      component: MyInformation
-    },
-    {
-      path: '/appointment',
-      name: 'Appointment',
-      component: Appointment
-    }
-  ]
+    routes: [
+        {
+            path: '/',
+            name: 'Hello',
+            component: Hello
+        },
+        {
+            path: '/pages/account/MyInformation',
+            name: 'MyInformation',
+            component: MyInformation
+        },
+        {
+            path: '/appointment',
+            name: 'Appointment',
+            component: Appointment
+        },
+        {
+            path: '/pages/login/login',
+            name: 'login',
+            component: Login
+        }
+    ]
 })
