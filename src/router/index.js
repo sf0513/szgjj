b9ef38fed4ecdf87aeb651a8a6d6741495f92976
licm2016 <525518330@qq.com>
2017-07-05 15:34:21 +0800

    licm2017

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,14 @@ import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import MyInformation from '../pages/account/MyInformation'
 import Appointment from '../pages/appointment/appointment'
+import ForgetPsw from '@/pages/psw/ForgetPsw'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/hh',
       name: 'Hello',
       component: Hello
     },
@@ -22,6 +23,12 @@ export default new Router({
       path: '/appointment',
       name: 'Appointment',
       component: Appointment
+    },
+    {
+      // path:'/forgetpsw',
+      path:'/',
+      name:'ForgetPsw',
+      component:ForgetPsw
     }
   ]
 })
