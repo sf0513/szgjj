5bdce7e268e8a9f1578f392f6a5bb4482425969f
llbt <llbt@llbts-MacBook-Pro.local>
2017-07-05 10:54:10 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import myAccount from '@/pages/account/myAccount.vue'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+        path: '/myAccount',
+        name: 'myAccount',
+        component: myAccount
     }
   ]
 })
