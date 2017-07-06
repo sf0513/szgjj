6eabfa1d44f9a64d0b0dfc655cabd835bb9b6d8e
luojian <luojian19910119@gmail.com>
2017-07-06 14:43:16 +0800

    修改登录界面，在主界面添加入口

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,67 +1,71 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+        <h2>Essential Links</h2>
 
-    <button @click="goToMyInformation">Go to MyInformation</button>
-    <br>
-    <button @click="goToMyAccount">Go to MyAccount</button>
+        <button @click="goToMyInformation">Go to MyInformation</button>
+        <br>
+        <button @click="goToMyAccount">Go to MyAccount</button>
+        <br/>
+        <button @click="goToLogin">Go to Login</button>
+        <!--<ul>-->
+        <!--<li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>-->
+        <!--<li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>-->
+        <!--<li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>-->
+        <!--<li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>-->
+        <!--<br>-->
+        <!--<li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>-->
+        <!--</ul>-->
+        <!--<h2>Ecosystem</h2>-->
+        <!--<ul>-->
+        <!--<li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>-->
+        <!--<li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>-->
+        <!--<li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>-->
+        <!--<li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>-->
+        <!--</ul>-->
 
-    <!--<ul>-->
-      <!--<li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>-->
-      <!--<li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>-->
-      <!--<li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>-->
-      <!--<li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>-->
-      <!--<br>-->
-      <!--<li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>-->
-    <!--</ul>-->
-    <!--<h2>Ecosystem</h2>-->
-    <!--<ul>-->
-      <!--<li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>-->
-      <!--<li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>-->
-      <!--<li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>-->
-      <!--<li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>-->
-    <!--</ul>-->
-
-  </div>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    export default {
+        name: 'hello',
+        data () {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        },
+        methods: {
+            goToMyInformation: function () {
+                this.$router.push('/pages/account/MyInformation')
+            },
+            goToMyAccount () {
+                this.$router.push('/pages/account/myAccount')
+            },
+            goToLogin(){
+                this.$router.push("pages/login/login")
+            }
+        }
     }
-  },
-  methods: {
-    goToMyInformation: function () {
-      this.$router.push('/pages/account/MyInformation')
-    },
-    goToMyAccount () {
-          this.$router.push('/pages/account/myAccount')
-    }
-  }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+    h1, h2 {
+        font-weight: normal;
+    }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
 
-a {
-color: #42b983;
-}
+    a {
+        color: #42b983;
+    }
 </style>
--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -2,7 +2,24 @@
     <div class="login">
         <h1>login</h1>
         <!--<img  src="@/assets/logo.png"/>-->
-        <input type="text">
+        <br/>
+        <div class="login_row">
+            <p class="login_name">账户：</p>
+            <input type="text" class="login_input">
+        </div>
+        <div class="login_row">
+            <p>密码：</p>
+            <input type="text" class="login_input">
+        </div>
+        <div class="login_row">
+            <p>验证码：</p>
+            <input type="text" class="login_input name">
+            <img>
+        </div>
+        <button class="login_submit">登录</button>
+        <div class="forget">
+            <router-link to=""><p>忘记密码</p></router-link>
+        </div>
     </div>
 </template>
 <script>
@@ -16,5 +33,43 @@
 </script>
 
 <style scoped>
+    .login {
+        background-color: #EFEFEF;
+        height: 100%;
+    }
 
+    .login_row {
+        width: 7rem;
+     }
+
+    .login_input {
+        height: 32px;
+        width: 6rem;
+    }
+
+    .login_input.name {
+        width: 3rem;
+        left:0px;
+     }
+
+    .login_submit {
+        position: relative;
+        width: 50%;
+        height: 32px;
+        background: #01AFEC;
+        border: none;
+        margin: 10px 20px 10px;
+        -webkit-transition: width 0.3s 0.15s, font-size 0.1s 0.15s;
+        transition: width 0.3s 0.15s, font-size 0.1s 0.15s;
+        border-radius: 3rem;
+        cursor: pointer;
+        color: white;
+        overflow: hidden;
+    }
+    .login_name{
+        position: absolute;
+        margin-left: 0.8rem;
+        height: 100%;
+        text-align: center;
+    }
 </style>
