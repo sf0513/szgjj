a970025fc319bd0d46602e1c6011c17ee130af5c
zyh <jt_zyh10000@163.com>
2017-07-07 16:46:08 +0800

    update

--- src/components/top-bar.vue
+++ src/components/top-bar.vue
@@ -12,8 +12,8 @@
 	
 </script>
 <style type="text/css">
-	.top-bar{margin-top:1.28rem; width: 7.5rem; height: 0.7rem; line-height: 0.7rem; background: url(../assets/top_bar_bg.jpg) no-repeat;}
+	.top-bar{margin-top:1.28rem; height: 0.7rem; line-height: 0.7rem; background: url(../assets/top_bar_bg.jpg) ; display: -webkit-flex; display: flex;}
 	.top-bar img{width: 0.48rem; height: 0.48rem; float: left; margin: 0.11rem 0.11rem 0 0.2rem;}
-	.top-bar span{font-size: 0.26rem; float: left; color: #fff;}
-	.top-bar input{width: 0.8rem; height: 0.4rem; float: right; background: #00a0e9; font-size: 0.2rem; color: #fff; border-radius: 0.2rem; margin:0.16rem 0.2rem 0 0; box-shadow: 0.05rem 0.05rem 0.05rem #00419d; outline: none;}
+	.top-bar span{font-size: 0.26rem; color: #fff; flex: 1; -webkit-flex: 1; text-align: left;}
+	.top-bar input{width: 0.8rem; height: 0.4rem; float: right; background: #00a0e9; font-size: 0.2rem; color: #fff; border-radius: 0.2rem; margin:0.16rem 0.2rem 0 0; box-shadow: 0.05rem 0.05rem 0.05rem #00419d;}
 </style>
