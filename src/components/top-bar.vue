0e938bfab182cfaa84737b16fa10617ab2587b3b
llbt <llbt@llbts-MacBook-Pro.local>
2017-07-11 11:46:04 +0800

    update

--- src/components/top-bar.vue
+++ src/components/top-bar.vue
@@ -12,7 +12,7 @@
 	
 </script>
 <style type="text/css">
-	.top-bar{margin-top:1.28rem; height: 0.7rem; line-height: 0.7rem; background: url(../assets/top_bar_bg.jpg) ; display: -webkit-flex; display: flex;}
+	.top-bar{height: 0.7rem; line-height: 0.7rem; background: url(../assets/top_bar_bg.jpg) ; display: -webkit-flex; display: flex;}
 	.top-bar img{width: 0.48rem; height: 0.48rem; float: left; margin: 0.11rem 0.11rem 0 0.2rem;}
 	.top-bar span{font-size: 0.26rem; color: #fff; flex: 1; -webkit-flex: 1; text-align: left;}
 	.top-bar input{width: 0.8rem; height: 0.4rem; float: right; background: #00a0e9; font-size: 0.2rem; color: #fff; border-radius: 0.2rem; margin:0.16rem 0.2rem 0 0; box-shadow: 0.05rem 0.05rem 0.05rem #00419d;}
