30458f12ba7288bd140280420e720a30994ce5bc
LiXuefei <LF@llbt.local>
2017-07-14 10:44:36 +0800

    by lixuefei

--- src/router/index.js
+++ src/router/index.js
@@ -28,7 +28,7 @@ Vue.use(Router)
 export default new Router({
     routes: [
         {
-            path: '/',
+            path: '/Hello',
             name: 'Hello',
             component: Hello
         },
@@ -118,7 +118,7 @@ export default new Router({
             component: modeOfRepayment
         },
         {
-            path: '/HedgeSign',
+            path: '/',
             name: 'HedgeSign',
             component: HedgeSign
         },
