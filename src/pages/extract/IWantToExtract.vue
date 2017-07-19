1b23df6644fdefe98165f38689c7d0a9a690988e
luojian <luojian19910119@gmail.com>
2017-07-19 16:42:10 +0800

    '我要提取界面添加对话框'

--- src/pages/extract/IWantToExtract.vue
+++ src/pages/extract/IWantToExtract.vue
@@ -40,10 +40,10 @@
                             </div>
                         </div>
                         <div class="column_two">
-                            <button v-if="item.able==='1'" class="able" @click="operate(item.operate)">
+                            <button v-if="item.able==='1'" class="able" @click="operate(item)">
                                 {{item.operate}}
                             </button>
-                            <button v-else-if="item.able='2'" disabled class="disable" @click="operate(item.operate)">
+                            <button v-else-if="item.able='2'" disabled class="disable" @click="operate(item)">
                                 {{item.operate}}
                             </button>
                         </div>
@@ -71,10 +71,10 @@
                             </div>
                         </div>
                         <div class="column_two">
-                            <button v-if="item.able==='1'" class="able" @click="operate(item.operate)">
+                            <button v-if="item.able==='1'" class="able" @click="operate(item)">
                                 {{item.operate}}
                             </button>
-                            <button v-else-if="item.able='2'" disabled class="disable" @click="operate(item.operate)">
+                            <button v-else-if="item.able='2'" disabled class="disable" @click="operate(item)">
                                 {{item.operate}}
                             </button>
                         </div>
@@ -84,6 +84,18 @@
                 <p class="line"></p>
             </div>
         </div>
+        <div class="pop" v-if='showFlag'>
+            <div class="confirm">
+                <div class="title"></div>
+                <div class="cont">
+                    <div class="tip">{{tip}}</div>
+                    <input type="button" value="确 定" class="sure fl" @click='quit()'>
+                    <input type="button" value="取 消" class="cancel fr" @click='showFlag=false'>
+                </div>
+            </div>
+            <div class="overlay"></div>
+        </div>
+
     </div>
 </template>
 
@@ -95,6 +107,8 @@
         data () {
             return {
                 show_item: "index_first",
+                showFlag: false,
+                tip: "",
                 immediateExtractionArr: [
                     {
                         type: "租 房",
@@ -232,7 +246,8 @@
                 }
             },
             operate(str){
-                alert(str)
+                this.tip = "你是否确认" + str.operate;
+                this.showFlag = true;
             }
         }
     }
@@ -512,4 +527,64 @@
         margin-right: 0.1rem;
     }
 
+    .overlay {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(0, 0, 0, 0.2);
+        z-index: 200;
+    }
+
+    .confirm {
+        position: fixed;
+        top: 45%;
+        left: 50%;
+        width: 7.12rem;
+        height: 3.56rem;
+        visibility: visible;
+        -webkit-transform: translate(-50%, -50%);
+        -ms-transform: translate(-50%, -50%);
+        transform: translate(-50%, -50%);
+        z-index: 201;
+        border-radius: 0.1rem;
+        background: white;
+        box-shadow: 0.03rem 0.03rem 0.03rem white;
+    }
+
+    .confirm .title {
+        height: 0.6rem;
+        background: url(../../assets/pop_bg.jpg) repeat-x;
+        border-radius: 0.1rem;
+    }
+
+    .confirm .cont {
+        height: 2.96rem;
+    }
+
+    .confirm .cont .tip {
+        height: 2rem;
+        font-size: 0.34rem;
+        line-height: 2rem;
+    }
+
+    .confirm .cont input {
+        width: 1.4rem;
+        height: 0.6rem;
+        background: #00afec;
+        border-radius: 0.22rem;
+        color: #fff;
+        font-size: 0.32rem;
+    }
+
+    .confirm .cont .sure {
+        margin-left: 1.8rem;
+    }
+
+    .confirm .cont .cancel {
+        margin-right: 1.8rem;
+        background: #DFDFDF;
+        color: black;
+    }
 </style>
