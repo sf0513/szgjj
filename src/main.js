d7eacdca824e37cd858d8cb8a3028aa9eb653ff7
zyh <jt_zyh10000@163.com>
2017-07-24 09:51:53 +0800

    update

--- src/main.js
+++ src/main.js
@@ -3,10 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import AMap from 'vue-amap'
 
 
 Vue.config.productionTip = false
 
+Vue.use(AMap);
+AMap.initAMapApiLoader({
+  key: 'your amap key',
+  plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor']
+});
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
