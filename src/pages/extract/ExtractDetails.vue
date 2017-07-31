01d2f7763b669f1ef2d977d0039451d94a85a01d
luojian <luojian19910119@gmail.com>
2017-07-31 14:51:59 +0800

    '布局详情界面添加按钮'

--- src/pages/extract/ExtractDetails.vue
+++ src/pages/extract/ExtractDetails.vue
@@ -44,14 +44,47 @@
                     <input value="20,000.00">
                     <img class="delete" src="../../assets/card_delete_1@2x.png">
                 </div>
+                <div class="phone">
+                    <span>请确认您正在使用的手机：</span>
+                    <input value="13800000000">
+                    <img class="delete" src="../../assets/card_delete_1@2x.png">
+                </div>
+                <button class="button" @click="extract">提取</button>
             </div>
             <div class="content_item" v-show="is_show_content_two">
-                <div class="step">
-                    <div class="step_one">
+                <div class="top">
+                    <div class="step">
+                        <div class="step_one item unchecked">
+                            <div class="unchecked circle">
+                                <img src="../../assets/card_steps_complete@2x.png">
+                             </div>
+                        </div>
+                        <div class="step_two item selected">
+                            <div class="selected circle">
+                                <img src="../../assets/card_steps_s@2x.png">
+                                <span>2</span>
+                            </div>
+                        </div>
+                        <div class="step_three item unchecked">
+                            <div class="unchecked circle">
+                                <img src="../../assets/card_steps_n@2x.png">
+                                <span>3</span>
+                            </div>
+                        </div>
 
                     </div>
-                    <div class="step_two"></div>
-                    <div class="step_three"></div>
+                    <div class="step">
+                        <div class="step_one item unchecked">
+                            <span>提取金额</span>
+                        </div>
+                        <div class="step_two item selected">
+                            <span>信息确认</span>
+                        </div>
+                        <div class="step_three item unchecked">
+                            <span>提取结果</span>
+                        </div>
+
+                    </div>
                 </div>
             </div>
             <div class="content_item" v-show="is_show_content_three"></div>
@@ -69,6 +102,13 @@
                 is_show_content_one: true,
                 is_show_content_two: false,
                 is_show_content_three: false
+            }
+        },
+        methods: {
+            extract(){
+                this.is_show_content_one = false;
+                this.is_show_content_two = true;
+                this.is_show_content_three = false;
             }
         },
         components: {
@@ -126,10 +166,9 @@
 
     }
 
-     .circle span {
+    .circle span {
         width: 50%;
         height: 50%;
-        /*overflow: auto;*/
         margin: auto;
         position: absolute;
         top: 0;
@@ -155,6 +194,11 @@
 
     .amount_money {
         background-color: white;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        font-weight: bold;
         width: 5rem;
     }
 
@@ -165,5 +209,48 @@
 
     .delete {
         width: 0.4rem;
+        height: 0.4rem;
+        margin-left: 0.1rem;
+    }
+
+    .phone {
+        background-color: white;
+        display: flex;
+        flex-direction: row;
+        font-size: 0.32rem;
+        margin-top: 0.8rem;
+        align-items: center;
+        justify-content: center;
+        height: 1.3rem;
+        width: 100%;
+    }
+
+    .phone input {
+        width: 2rem;
+        color: blue;
+        text-decoration: underline;
+    }
+
+    input, button {
+        outline: none;
+        border: none;
+    }
+
+    .button {
+        position: relative;
+        width: 2.6rem;
+        height: 35px;
+        font-size: 16px;
+        background-image: url(../../assets/btn.png);
+        margin: 48px 20px 10px;
+        -webkit-transition: width 0.3s 0.15s, font-size 0.1s 0.15s;
+        transition: width 0.3s 0.15s, font-size 0.1s 0.15s;
+        border-radius: 3rem;
+        cursor: pointer;
+        color: white;
+        background-repeat: no-repeat;
+        background-position: 50% center;
+        background-color: transparent;
+        overflow: hidden;
     }
 </style>
