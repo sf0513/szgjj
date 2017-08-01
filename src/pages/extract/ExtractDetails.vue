d2df9319fed54b046ada65dbbc74901046c5e4ec
luojian <luojian19910119@gmail.com>
2017-08-01 17:08:09 +0800

    '添加提取详情第三步骤，添加部分逻辑功能'

--- src/pages/extract/ExtractDetails.vue
+++ src/pages/extract/ExtractDetails.vue
@@ -1,7 +1,6 @@
 <template xmlns:v-bind="http://www.w3.org/1999/xhtml">
     <div class="extract_details">
         <NavHeader :title="title" :showImg="false" :isshowcolor="true"></NavHeader>
-
         <div class="content">
             <div class="content_item" v-show="is_show_content_one">
                 <div class="top">
@@ -41,17 +40,17 @@
                 </div>
                 <div class="amount_money">
                     <span class="money_num">￥</span>
-                    <input class="money_num" v-model="money">
-                    <img class="delete" src="../../assets/card_delete_1@2x.png">
+                    <input class="money_num" v-model.number="money" type="number">
+                    <img class="delete" @click="clearMoney" src="../../assets/card_delete_1@2x.png">
                 </div>
                 <div class="phone bg_item">
                     <span>请确认您正在使用的手机：</span>
                     <input v-model="phone">
-                    <img class="delete" src="../../assets/card_delete_1@2x.png">
+                    <img class="delete" @click="clearPhoneNumber" src="../../assets/card_delete_1@2x.png">
                 </div>
-                <div class="rule" v-show="is_show_rule">
+                <div class="rule" v-if="is_show_rule">
                     <div>
-                        <span>本次最高可提取</span><span class="highest_money">20,000.00</span><span>元</span>
+                        <span>本次最高可提取</span><span class="highest_money">{{totalMoney}}</span><span>元</span>
                         <span class="calculation_rule">查看计算规则</span>
                     </div>
                     <div>
@@ -120,8 +119,24 @@
                 </div>
             </div>
             <div class="content_item" v-show="is_show_content_three">
-                <img :src="src"/>
-
+                <img class="success_sign" :src="src"/>
+                <span class="success_remind">恭喜你，受理成功！</span>
+                <div class="success_rule_remind">
+                    <div><span>资金将在</span><span class="important_remind">3个工作日</span><span>内转入您的联名卡</span></div>
+                    <div class="remind_two"><span>在资金转入前</span><span
+                            class="important_remind">请勿</span><span>将联名卡注销</span></div>
+                </div>
+                <div class="bg_item bg_item_font">
+                    <span>提取金额：</span>
+                    <span class="money_num">￥{{money}}</span>
+                </div>
+                <div class="bg_item bg_item_font">
+                    <span>账户余额：</span>
+                    <span class="money_num">￥{{accountBalance}}</span>
+                </div>
+                <div class="operate">
+                    <button class="my_button one" @click="finish">完&nbsp;&nbsp;&nbsp;&nbsp;成</button>
+                </div>
             </div>
         </div>
     </div>
@@ -133,32 +148,77 @@
         name: "ExtractDetails",
         data () {
             return {
-                title: '租房提取',
+                title: '',
                 is_show_content_one: true,
                 is_show_content_two: false,
                 is_show_content_three: false,
                 is_show_rule: false,
                 src: require('../../assets/appoint_success.png'),
-                money: 20000,
+                totalMoney: 25000,
+                money: '',
                 phone: 13800000000,
 
             }
         },
+        created: function () {
+            //从上一个页面来的数据
+            var typeSign = this.$route.query.typeSign;
+            this.money = this.totalMoney;
+            switch (typeSign) {
+                case 1:
+                    this.title = "租房提取";
+                    this.is_show_rule = true;
+                    break;
+                case 2:
+                    this.title = "其他住房消费提取";
+                    this.is_show_rule = true;
+                    break;
+                case 3:
+                    this.title = "非深户离深销户提取";
+                    this.is_show_rule = false;
+                    break;
+                case 4:
+                    this.title = "退休销户提取";
+                    this.is_show_rule = false;
+                    break;
+            }
+
+        },
+        computed: {
+            //账户余额
+            accountBalance: function () {
+                return this.totalMoney - this.money;
+            }
+        },
         methods: {
-            extract(){
+            //提取
+            extract: function () {
                 this.is_show_content_one = false;
                 this.is_show_content_two = true;
                 this.is_show_content_three = false;
             },
-            previousStep(){
+            //上一步
+            previousStep: function () {
                 this.is_show_content_one = true;
                 this.is_show_content_two = false;
                 this.is_show_content_three = false;
             },
-            sure(){
+            //确认
+            sure: function () {
                 this.is_show_content_one = false;
                 this.is_show_content_two = false;
                 this.is_show_content_three = true;
+            },
+            //完成
+            finish: function () {
+                history.back();
+
+            },
+            clearMoney: function () {
+                this.money='';
+            },
+            clearPhoneNumber:function () {
+                this.phone='';
             }
         },
         components: {
@@ -172,8 +232,7 @@
     .extract_details {
         width: 100%;
         height: 100%;
-        /*position: relative;*/
-        background-repeat: repeat-x;
+        position: absolute;
         background-color: #EFEFEF;
         font-size: 0.38rem;
     }
@@ -251,6 +310,7 @@
         justify-content: center;
         margin-bottom: 0.5rem;
         width: 5rem;
+        box-shadow: 1px 1px #949494;
     }
 
     .amount_money input {
@@ -344,7 +404,7 @@
     .my_button {
         width: 3rem;
         height: 0.8rem;
-        font-size: 16px;
+        font-size: 0.37rem;
         -webkit-transition: width 0.3s 0.15s, font-size 0.1s 0.15s;
         transition: width 0.3s 0.15s, font-size 0.1s 0.15s;
         border-radius: 3rem;
@@ -364,5 +424,31 @@
     .two {
         background-image: url(../../assets/bg_extract_details_two.png);
         color: black;
+    }
+
+    .success_sign {
+        margin-top: 1.3rem;
+        width: 1.8rem;
+        height: 1.8rem;
+    }
+
+    .success_remind {
+        margin-top: 0.5rem;
+        font-size: 0.35rem;
+        font-weight: bold;
+    }
+
+    .success_rule_remind {
+        font-size: 0.25rem;
+        margin-top: 0.4rem;
+    }
+
+    .important_remind {
+        color: red;
+        font-size: 0.28rem;
+    }
+
+    .remind_two {
+        margin-top: 0.1rem;
     }
 </style>
